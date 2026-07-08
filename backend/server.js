b391402a6a88a20a6ca39dbeb07fa16439c05636
Etorua <153048739+Etorua@users.noreply.github.com>
2026-07-08 08:56:23 -0700

    Fix Express wildcard route syntax

--- backend/server.js
+++ backend/server.js
@@ -196,7 +196,7 @@ app.post('/api/settings', async (req, res) => {
 
 // --- SERVIR EL FRONTEND (PÁGINA WEB) ---
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
